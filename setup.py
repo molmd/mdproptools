b956822b3d083aa65f1e9a53c7de3951578509a2
Rasha <atwi.r@husky.neu.edu>
2021-07-30 00:16:48 -0400

    Package mdproptools

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+
+import setuptools
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+setuptools.setup(
+    name="mdproptools",
+    version="0.0.1",
+    author="Rasha Atwi, Matthew Bliss, Maxim Makeev",
+    author_email="rasha.atwi@stonybrook.edu, matthew.bliss@stonybrook.edu, "
+                 "maxim.makeev@stonybrook.edu",
+    description="mdproptools contains MD analysis tools",
+    long_description=open(os.path.join(module_dir, "README.md")).read(),
+    url="https://github.com/tufts-university-rajput-lab/mdproptools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status ::  2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    python_requires=">=3.6",
+    package_data={},
+)
