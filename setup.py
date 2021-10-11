fad5fac9f74f83d766d9a71ba8f23f44364d383b
rashatwi <atwi.r@husky.neu.edu>
2021-10-11 10:44:19 -0400

    Update mdproptools version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ module_dir = os.path.dirname(os.path.abspath(__file__))
 
 setuptools.setup(
     name="mdproptools",
-    version="0.0.1",
+    version="0.0.2",
     author="Rasha Atwi, Matthew Bliss, Maxim Makeev",
     author_email="rasha.atwi@stonybrook.edu, matthew.bliss@stonybrook.edu, "
                  "maxim.makeev@stonybrook.edu",
