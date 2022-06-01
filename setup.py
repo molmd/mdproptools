be34b0ed0c5337f6ac1b41097f97c43b5be2dc68
rashatwi <atwi.r@husky.neu.edu>
2022-06-01 17:59:10 -0400

    Update installation requirements

--- setup.py
+++ setup.py
@@ -6,17 +6,26 @@ module_dir = os.path.dirname(os.path.abspath(__file__))
 
 setuptools.setup(
     name="mdproptools",
-    version="0.0.2",
+    version="0.0.3",
     author="Rasha Atwi, Matthew Bliss, Maxim Makeev",
     author_email="rasha.atwi@stonybrook.edu, matthew.bliss@stonybrook.edu, "
                  "maxim.makeev@stonybrook.edu",
     description="mdproptools contains MD analysis tools",
     long_description=open(os.path.join(module_dir, "README.md")).read(),
-    url="https://github.com/tufts-university-rajput-lab/mdproptools",
+    url="https://github.com/molmd/mdproptools",
+    install_requires=["numpy >= 1.21.1",
+                      "matplotlib >= 3.3.1",
+                      "scipy >= 1.5.2",
+                      "pandas >= 1.1.2",
+                      "seaborn >= 0.11.0",
+                      "statsmodels >= 0.12.0",
+                      "numba == 0.53.1",
+                      "llvmlite == 0.36.0"
+                      ],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Development Status ::  2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Information Technology",
         "Operating System :: OS Independent",
