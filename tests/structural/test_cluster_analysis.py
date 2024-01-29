3282df26e8d0521cecd590ac047f5327101e84b7
Rasha Atwi <atwi.r@husky.neu.edu>
2024-01-29 11:49:21 -0500

    Use Path for constructing file paths

--- tests/structural/test_cluster_analysis.py
+++ tests/structural/test_cluster_analysis.py
@@ -16,7 +16,7 @@ class TestClusterAnalysis(unittest.TestCase):
     def setUp(self):
         self.test_dir = Path(__file__).parent / "test_files"
         self.data_dir = Path(__file__).resolve().parents[2] / "data" / "structural"
-        self.dump_files = os.path.join(self.data_dir, "Mg_2TFSI_G1.lammpstrj.*")
+        self.dump_files = str(self.data_dir / "Mg_2TFSI_G1.lammpstrj.*")
         self.elements = ["O", "C", "H", "N", "S", "O", "F", "Mg"]
         self.r_cut = 2.3
         self.max_force = 0.75
@@ -40,9 +40,9 @@ class TestClusterAnalysis(unittest.TestCase):
         num_clusters = self.run_get_clusters(32, True)
         self.assertEqual(num_clusters, 33)
 
-        dme = Molecule.from_file(os.path.join(self.data_dir, "dme.pdb"))
-        tfsi = Molecule.from_file(os.path.join(self.data_dir, "tfsi.pdb"))
-        mg = Molecule.from_file(os.path.join(self.data_dir, "mg.pdb"))
+        dme = Molecule.from_file(str(self.data_dir / "dme.pdb"))
+        tfsi = Molecule.from_file(str(self.data_dir / "tfsi.pdb"))
+        mg = Molecule.from_file(str(self.data_dir / "mg.pdb"))
 
         clusters_df_test, conf_df_test = get_unique_configurations(
             cluster_pattern="Cluster_*.xyz",
@@ -58,11 +58,11 @@ class TestClusterAnalysis(unittest.TestCase):
         )
         self.compare_generated_xyz_files("conf_*.xyz")
 
-        cluster_df = pd.read_csv(os.path.join(self.test_dir, "clusters.csv"))
+        cluster_df = pd.read_csv(str(self.test_dir / "clusters.csv"))
         cluster_df.fillna("", inplace=True)
         pd.testing.assert_frame_equal(clusters_df_test, cluster_df, check_dtype=False)
 
-        top_conf_df = pd.read_csv(os.path.join(self.test_dir, "top_conf.csv"))
+        top_conf_df = pd.read_csv(str(self.test_dir / "top_conf.csv"))
         top_conf_df_test = pd.read_csv("top_conf.csv")
         self.assertEqual(len(top_conf_df_test), 3)
         pd.testing.assert_frame_equal(top_conf_df_test, top_conf_df)
@@ -86,7 +86,7 @@ class TestClusterAnalysis(unittest.TestCase):
 
     def compare_generated_xyz_files(self, pattern):
         xyz_files_test = sorted(glob.glob(pattern))
-        xyz_files = sorted(glob.glob(os.path.join(self.test_dir, pattern)))
+        xyz_files = sorted(glob.glob(str(self.test_dir / pattern)))
         self.assertEqual(len(xyz_files_test), len(xyz_files))
         for file1, file2 in zip(xyz_files_test, xyz_files):
             are_same = filecmp.cmp(file1, file2, shallow=False)
