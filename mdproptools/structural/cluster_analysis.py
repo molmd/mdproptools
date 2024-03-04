e1caa6ee4ac96de147428a782dffb4065e758c81
Rasha Atwi <atwi.r@husky.neu.edu>
2024-03-04 13:35:08 -0500

    Sort clusters dataframes by name to ensure consistent top conf

--- mdproptools/structural/cluster_analysis.py
+++ mdproptools/structural/cluster_analysis.py
@@ -450,7 +450,7 @@ def get_unique_configurations(
                 "configurations"
             )
         merge_cols = [i for i in list(df.columns) if i.startswith("atoms_")]
-        print(df)
+        df = df.sort_values("cluster")
         top_config = top_config.merge(
             df[["cluster"] + merge_cols], on=merge_cols
         ).drop_duplicates(merge_cols)
